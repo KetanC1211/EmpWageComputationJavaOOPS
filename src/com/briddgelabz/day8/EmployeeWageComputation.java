7032694010deb99da231035b2a1ce3e6500de91c
KetanC1211 <ketanchindarkar12@gmail.com>
2022-07-14 00:54:31 +0530

    refactored UC-2

--- src/com/briddgelabz/day8/EmployeeWageComputation.java
+++ src/com/briddgelabz/day8/EmployeeWageComputation.java
@@ -1,11 +1,18 @@
 package com.briddgelabz.day8;
 
 public class EmployeeWageComputation {
+	public static final int IS_FULL_TIME=2;
+	public static final int IS_PART_TIME=1;
 	EmployeeWageComputation(int x){
 		int empRatePerHrs=20;
-		int empHrs=8;
 		int salary;
-		if (x==1) {
+		if (x==IS_FULL_TIME) {
+			int empHrs=8;
+			salary = empRatePerHrs*empHrs;
+			System.out.println("Employee Daily Wage is "+salary);
+		}
+		else if(x==IS_PART_TIME){
+			int empHrs=4;
 			salary = empRatePerHrs*empHrs;
 			System.out.println("Employee Daily Wage is "+salary);
 		}
@@ -15,7 +22,7 @@ public class EmployeeWageComputation {
 	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
-		int empCheck = (int)Math.floor(Math.random()*10%2);
+		int empCheck = (int)Math.floor(Math.random()*10%3);
 		EmployeeWageComputation obj = new EmployeeWageComputation(empCheck);
 	}
 }
