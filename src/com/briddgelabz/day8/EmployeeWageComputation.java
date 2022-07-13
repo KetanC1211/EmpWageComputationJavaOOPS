cd94b87cd96c2de115b3f4b030d9ad2ba35eb9d7
KetanC1211 <ketanchindarkar12@gmail.com>
2022-07-14 01:04:46 +0530

    refactored UC-4

--- src/com/briddgelabz/day8/EmployeeWageComputation.java
+++ src/com/briddgelabz/day8/EmployeeWageComputation.java
@@ -3,26 +3,31 @@ package com.briddgelabz.day8;
 public class EmployeeWageComputation {
 	public static final int IS_FULL_TIME=2;
 	public static final int IS_PART_TIME=1;
-	EmployeeWageComputation(int x){
+	public static final int NUM_OF_WORKING_DAY=20;
+	EmployeeWageComputation(){
 		int empRatePerHrs=20;
+		int totalSalary=0;
 		int salary;
 		int empHrs;
-		switch(x)	{
-			case IS_FULL_TIME:
-				empHrs = 8;
-				break;
-			case IS_PART_TIME:
-				empHrs = 4;
-				break;
-			default:
-				empHrs =0;
+		for(int i=1;i<=NUM_OF_WORKING_DAY;i++){ 
+			int empCheck = (int)Math.floor(Math.random()*10)%3;
+			switch(empCheck)	{
+				case IS_FULL_TIME:
+					empHrs = 8;
+					break;
+				case IS_PART_TIME:
+					empHrs = 4;
+					break;
+				default:
+					empHrs =0;
+			}
+			salary = empRatePerHrs*empHrs;
+			totalSalary = totalSalary + salary;
 		}
-		salary = empRatePerHrs*empHrs;
-		System.out.println("Daily Wage is "+ salary);
+		System.out.println("Monthly Wage is "+ totalSalary);
 	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
-		int empCheck = (int)Math.floor(Math.random()*10%3);
-		EmployeeWageComputation obj = new EmployeeWageComputation(empCheck);
+		EmployeeWageComputation obj = new EmployeeWageComputation();
 	}
 }
