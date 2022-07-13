7b5a9fbfced06de28fbc7f2bedd2eb35e1ac1ca6
KetanC1211 <ketanchindarkar12@gmail.com>
2022-07-14 00:49:08 +0530

    Added EmployeeAttendance

--- src/com/briddgelabz/day8/EmployeeWageComputation.java
+++ src/com/briddgelabz/day8/EmployeeWageComputation.java
@@ -1,10 +1,19 @@
 package com.briddgelabz.day8;
 
 public class EmployeeWageComputation {
-
-	public static void main(String[] args) {
+	EmployeeWageComputation(int x){
+		if (x==0) {
+			System.out.println("Employee is Absent");
+		}
+		else {
+			System.out.println("Employee is Present");
+		}
+	}
 	
-
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Program");
+		int empCheck = (int)Math.floor(Math.random()*10%2);
+		EmployeeWageComputation obj = new EmployeeWageComputation(empCheck);
 	}
 
 }
