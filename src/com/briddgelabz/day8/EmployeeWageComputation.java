814f34b33dd1a2c63905a53ec35015bc204dc8df
KetanC1211 <ketanchindarkar12@gmail.com>
2022-07-14 00:44:47 +0530

    Refactored UC1

--- src/com/briddgelabz/day8/EmployeeWageComputation.java
+++ src/com/briddgelabz/day8/EmployeeWageComputation.java
@@ -1,10 +1,21 @@
 package com.briddgelabz.day8;
 
 public class EmployeeWageComputation {
-
-	public static void main(String[] args) {
-	
-
+	EmployeeWageComputation(int x){
+		int empRatePerHrs=20;
+		int empHrs=8;
+		int salary;
+		if (x==1) {
+			salary = empRatePerHrs*empHrs;
+			System.out.println("Employee Daily Wage is "+salary);
+		}
+		else {
+			System.out.println("Employee Daily Wage is 0");
+		}
 	}
-
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Program");
+		int empCheck = (int)Math.floor(Math.random()*10%2);
+		EmployeeWageComputation obj = new EmployeeWageComputation(empCheck);
+	}
 }
