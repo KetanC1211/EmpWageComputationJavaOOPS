decf277bdfa0a28cf78c672784e835e5e5a729f6
KetanC1211 <ketanchindarkar12@gmail.com>
2022-07-25 20:56:03 +0530

    Added instance Variables

--- src/com/briddgelabz/assignment/EmployeeWageComputation.java
+++ src/com/briddgelabz/assignment/EmployeeWageComputation.java
@@ -1,10 +1,24 @@
 package com.briddgelabz.assignment;
 
 public class EmployeeWageComputation {
+	//Constants
 	public static final int IS_FULL_TIME=2;
 	public static final int IS_PART_TIME=1;
+	//Instance Variable
+	private String companyName;
+	private int empRatePerHour;
+	private int numOfWorkingDays;
+	private int maxHoursPerMonth;
+	private int totalEmpWage;
+
+	public EmployeeWageComputation(String companyName, int empRatePerHour , int numOfWorkingDays, int maxHoursPerMonth){
+		this.companyName = companyName;
+		this.empRatePerHour = empRatePerHour;
+		this.numOfWorkingDays = numOfWorkingDays;
+		this.maxHoursPerMonth = maxHoursPerMonth;
+	} 
 	
-	public static int employeeWageComputation(String companyName, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth){
+	public int computeEmpWage() {
 		int empHour = 0, totalEmpHours = 0, totalWorkingDays = 0;
 		while(totalEmpHours <=maxHoursPerMonth && totalWorkingDays< numOfWorkingDays){ 
 			int empCheck = (int)Math.floor(Math.random()*10)%3;
@@ -21,15 +35,20 @@ public class EmployeeWageComputation {
 			totalEmpHours = totalEmpHours + empHour;
 			totalWorkingDays++;
 			System.out.println("Day : "+totalWorkingDays+" Emp Hours: "+empHour);
-			
 		}
-		int totalEmpWage = totalEmpHours * empRatePerHour;
-		System.out.println("Total Emp Wage for Company: "+companyName+" is:"+totalEmpWage );
-		return totalEmpWage;
+		totalEmpWage = totalEmpHours * empRatePerHour;
+		//System.out.println("Total Emp Wage for Company: "+companyName+" is:"+totalEmpWage);
+		return totalEmpWage; 
 	}
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
-		employeeWageComputation("Dominos", 15, 25, 100);
-		employeeWageComputation("PizzaHut", 20, 20, 100);
+		EmployeeWageComputation dominos = new EmployeeWageComputation("Dominos", 15, 25, 100);
+		EmployeeWageComputation pizzaHut = new EmployeeWageComputation("PizzaHut", 20, 20, 100);
+		dominos.computeEmpWage();
+		System.out.println("Dominos Emp salary "+dominos.totalEmpWage);//saving salary
+		pizzaHut.computeEmpWage();
+		System.out.println("PizzaHut Emp salary "+pizzaHut.totalEmpWage);//saving salary
+		
 	}
 }
